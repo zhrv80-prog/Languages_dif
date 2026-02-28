64a6d5afd50922345e86478360458eb8e5b1fd89
zhrv80-prog <zhrv80@gmail.com>
2026-02-28 07:15:36 +0400

    Add Selenium test for product page button

--- test_items.py
+++ test_items.py
@@ -1,1 +1,26 @@
 #Тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину. Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+#Открыть страницу http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button1 = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket[value='Добавить в корзину']")  
+    #с ожиданием если button1 = WebDriverWait(browser, 10).until(
+    #EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket"))
+    #)
+    #Сам элемент вот такой <button type="submit" class="btn btn-lg btn-primary btn-add-to-basket" value="Добавить в корзину" data-loading-text="Добавление...">Добавить в корзину</button>
+    print(button1.text)
+    
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
