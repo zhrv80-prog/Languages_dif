e994a47f7913af7775ae0d70274e9db33bad41f6
zhrv80-prog <zhrv80@gmail.com>
2026-02-28 06:23:00 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,15 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 #Обработчик командной строки, который считывает из командной строки параметр language
 
-
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=en,
+                     help="Choose langauge")
 
 #Логика запуска браузера с указанным языком пользователя. Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
