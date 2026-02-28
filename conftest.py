b6f9af17115eedce0a1bdb98c880d382406ad54a
zhrv80-prog <zhrv80@gmail.com>
2026-02-28 11:37:03 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,26 +1,40 @@
-import pytest
+#Тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину. Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+import time
 
-#Обработчик командной строки, который считывает из командной строки параметр language
-#Тест должен запускаться с параметром language например командой: pytest --language=es test_items.py
+#Открыть страницу http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
 
-def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=en,
-                     help="Choose langauge")
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-#Логика запуска браузера с указанным языком пользователя. Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
+try:
     browser = webdriver.Chrome()
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-    yield browser
-    print("\nquit browser..")
+    browser.get(link)
+
+    try:
+        #Ищем элемент
+        button1 = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket[value='Добавить в корзину']")  
+        """
+        если с ожиданием  
+        button1 = WebDriverWait(browser, 10).until(
+                                                EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket"))
+                                                )
+        """
+        #Сам элемент вот такой 
+        #<button type="submit" class="btn btn-lg btn-primary btn-add-to-basket" value="Добавить в корзину" data-loading-text="Добавление...">Добавить в корзину</button>
+        
+        print(button1.text)
+
+        button_found = True
+    except NoSuchElementException:
+        button_found = False
+
+    assert button_found
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
     browser.quit()
 
-
+# не забываем оставить пустую строку в конце файла
