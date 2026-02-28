f0957023ce904e159c5eff5450c39b77f53a2eb5
zhrv80-prog <zhrv80@gmail.com>
2026-02-28 22:14:49 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,40 +1,24 @@
-#Тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину. Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.chrome.options import Options
 
-#Открыть страницу http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
+#Обработчик командной строки, который считывает из командной строки параметр language
+#Тест должен запускаться с параметром language например командой: pytest --language=es test_items.py
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=en,
+                     help="Choose langauge")
 
-try:
+#Логика запуска браузера с указанным языком пользователя. Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    browser.get(link)
-
-    try:
-        #Ищем элемент
-        button1 = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket[value='Добавить в корзину']")  
-        """
-        если с ожиданием  
-        button1 = WebDriverWait(browser, 10).until(
-                                                EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket"))
-                                                )
-        """
-        #Сам элемент вот такой 
-        #<button type="submit" class="btn btn-lg btn-primary btn-add-to-basket" value="Добавить в корзину" data-loading-text="Добавление...">Добавить в корзину</button>
-        
-        print(button1.text)
-
-        button_found = True
-    except NoSuchElementException:
-        button_found = False
-
-    assert button_found
-
-finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(30)
-    # закрываем браузер после всех манипуляций
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
     browser.quit()
-
-# не забываем оставить пустую строку в конце файла
