1388899f71b14fdeb57dc6af550827765e0827eb
zhrv80-prog <zhrv80@gmail.com>
2026-03-01 11:33:39 +0400

    Update conftest.py to handle language options
    Refactor browser fixture to accept language parameter and register language option.

--- conftest.py
+++ conftest.py
@@ -6,19 +6,28 @@ from selenium.webdriver.chrome.options import Options
 #Обработчик командной строки, который считывает из командной строки параметр language
 #Тест должен запускаться с параметром language например командой: pytest --language=es test_items.py
 
+#Регистрируем опцию language
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=en,
-                     help="Choose langauge")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
+
+#Получаем значение опции language
+@pytest.fixture()
+def language(request):
+    """Фикстура, возвращающая выбранный язык"""
+    return request.config.getoption('--language')
 
 #Логика запуска браузера с указанным языком пользователя. Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
 
-@pytest.fixture(scope="function")
-def browser():
+
+#Запускаем браузер с языком
+@pytest.fixture()
+def browser(language):
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
+
