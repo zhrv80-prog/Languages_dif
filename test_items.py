39e5f85b6b37352632a298b43ef8f578a0c91798
zhrv80-prog <zhrv80@gmail.com>
2026-03-01 11:32:14 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -9,34 +9,21 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-try:
-    browser = webdriver.Chrome()
+def test_but_add_cart_exists(browser):
     browser.get(link)
-
+    time.sleep(30)
     try:
         #Ищем элемент
-        button1 = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket[value='Добавить в корзину']")  
+        button1 = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")  
         """
         если с ожиданием  
         button1 = WebDriverWait(browser, 10).until(
                                                 EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket"))
                                                 )
         """
-        #Сам элемент вот такой 
-        #<button type="submit" class="btn btn-lg btn-primary btn-add-to-basket" value="Добавить в корзину" data-loading-text="Добавление...">Добавить в корзину</button>
-        
         print(button1.text)
-
         button_found = True
     except NoSuchElementException:
         button_found = False
 
     assert button_found
-
-finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
-
-# не забываем оставить пустую строку в конце файла
